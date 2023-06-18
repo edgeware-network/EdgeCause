af50b24183667af6618fb2f291b64839a4a95ba3
Shankar D. Warang <shankar@edgeware.agency>
2023-06-18 22:29:12 +0530

    Update hardhat.config.js for HackFS

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -2,6 +2,16 @@ module.exports = {
   solidity: {
     version: '0.8.9',
     networks: {
+      Filecoin: {
+        url: 'https://rpc.ankr.com/filecoin',
+        chainId: 314,
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      },
+      Calibration: {
+        url: 'https://filecoin-calibration.chainstacklabs.com/rpc/v1',
+        chainId: 314159,
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      },
       Edgeware: {
         url: 'https://edgeware-evm.jelliedowl.net',
         chainId: 2021,
